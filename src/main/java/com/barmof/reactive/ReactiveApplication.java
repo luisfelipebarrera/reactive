844c30f218cb9ffd6472ad3a46a4217c842ff2a5
Luis Felipe Barrera Mora <luisfelipe.barrera@gmail.com>
2024-12-21 10:57:42 -0500

    Flux method

--- src/main/java/com/barmof/reactive/ReactiveApplication.java
+++ src/main/java/com/barmof/reactive/ReactiveApplication.java
@@ -1,13 +1,31 @@
 package com.barmof.reactive;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class ReactiveApplication {
+public class ReactiveApplication implements CommandLineRunner {
 
+	private static final Logger log = LoggerFactory.getLogger(ReactiveApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Flux<String> names = Flux.just("Luis", "Felipe", "", "Barrera", "Mora")
+				.doOnNext(name -> {
+					if (name.isEmpty()) {
+						throw new RuntimeException("Name can't be empty");
+					}
+					System.out.println(name);
+				});
+		names.subscribe(e -> log.info(e),
+				error -> log.error(error.getMessage()));
+	}
 }
